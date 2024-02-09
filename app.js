00d91a56d241a11d24449f8961bbdd92e7c1bfb6
PRASHANT SINGH <146015140+prashantsingh151198@users.noreply.github.com>
2024-02-09 11:30:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+
+
+let canvas = document.querySelector('canvas');
+
+let ctx = canvas.getContext('2d');
+
+// ctx.fillStyle = 'red'
+
+// ctx.fillRect(200 , 200,100,100);
+
+// ctx.strokeRect(100,100,50,50);
+
+// ctx.strokeStyle = "purple"
+
+// ctx.strokeRect(100,100,50,50);
+
+// ctx.beginPath();
+// ctx.moveTo(20,30);
+// ctx.lineTo(120,130);
+// ctx.lineTo(20,130)
+// ctx.lineTo(20,30)
+// ctx.lineTo(120,30)
+// ctx.lineTo(120,95)
+// ctx.strokeStyle = 'red'
+// ctx.stroke();
+// ctx.fillStyle = 'blue'
+// ctx.fill()
+// ctx.closePath();
+
+//-------------------------------------------------
+
+ctx.font = "28px serif"
+ctx.fillStyle = 'red'
+ctx.fillText('prashant singh they are march batch' , 50 ,75)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
